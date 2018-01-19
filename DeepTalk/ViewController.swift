29efea8ba1acc508b27b41ae2aabaf37399d06fa
Martin Pietrowski <m.pietrowski@appcom-interactive.de>
2018-01-19 22:33:07 +0100

    Initial Commit

---
+++ DeepTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeepTalk
+//
+//  Created by Martin Pietrowski on 19.01.18.
+//  Copyright © 2018 devpie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
